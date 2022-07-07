a986b689190761e57c7e71a3eac2b94e463de683
AdriaRos <adria.ros@elma.care>
2022-07-07 10:27:24 +0200

    Added Dependencies

--- Marvelpop/Marvelpop/Core/Factory/DI/Protocol/DependencyFactory.swift
+++ Marvelpop/Marvelpop/Core/Factory/DI/Protocol/DependencyFactory.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 protocol DependencyFactory {
-    
+    var marvelAPI: MarvelAPIProtocol { get }
+    var charactersRepository: CharactersRepositoryProtocol { get }
+    var mediaRepository: MediaRepositoryProtocol { get }
+    var favouritesRepository: FavouritesRepositoryProtocol { get }
+    var imageLoaderUseCase: ImageLoaderUseCaseProtocol { get }
 }
